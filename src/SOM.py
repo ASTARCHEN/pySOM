b38e591814c7ba89c3e096f2c81b521f526046b0
ASTARCHEN <chenxiaolong12315@163.com>
2017-11-21 15:56:33 +0800

    pySOM v 0.0.2版本构建

--- src/SOM.py
+++ src/SOM.py
@@ -7,86 +7,108 @@ import numpy as np
 import cmath as math
 import pylab as plt
 import time
+import numpy as np
+from scipy.linalg import norm
+from scipy.spatial import distance as dist
+import itertools
+npa = np.array
 
-#初始化输入层与竞争层神经元的连接权值矩阵
-def initCompetition(n , m , d):
+def initCompetition(mat):
+    """
+    初始化输入层与竞争层神经元的连接权值矩阵
+    :param mat: tuple 类型，代表输入的维度
+    :return:
+    """
+    # 随机产生0-1之间的数作为权值
+    return np.random.random(mat)
 
-    #随机产生0-1之间的数作为权值
-    array = np.random.random(size=n * m *d)
-    com_weight = array.reshape(n,m,d)
-    return com_weight
-
-#计算向量的二范数
-def cal2NF(X):
-    res = 0
-    for x in X:
-        res += x*x
-    return res ** 0.5
-
-#对数据集进行归一化处理
 def normalize(dataSet):
-    old_dataSet = np.copy(dataSet)
-    for data in dataSet:
-        two_NF = cal2NF(data)
-        for i in range(len(data)):
-            data[i] = data[i] / two_NF
-    return dataSet , old_dataSet
-#对权值矩阵进行归一化处理
+    """
+    对数据集进行归一化处理
+    :param dataSet:
+    :return:
+    """
+    dataSet = npa(dataSet)
+    m, n = dataSet.shape
+    for i in range(m):
+        dataSet[i, :] = dataSet[i, :] / norm(dataSet[i, :], 2)
+    return dataSet
 def normalize_weight(com_weight):
-    for x in com_weight:
-        for data in x:
-            two_NF = cal2NF(data)
-            for i in range(len(data)):
-                data[i] = data[i] / two_NF
+    """
+    对权值矩阵进行归一化处理
+    :param com_weight:
+    :return:
+    """
+    m = com_weight.shape[0]
+    for ix in range(m):
+        com_weight[ix] = normalize(com_weight[ix])
     return com_weight
 
-#得到获胜神经元的索引值
-def getWinner(data , com_weight):
-    max_sim = 0
-    n,m,d = np.shape(com_weight)
-    mark_n = 0
-    mark_m = 0
-    for i in range(n):
-        for j in range(m):
-            if sum(data * com_weight[i,j]) > max_sim:
-                max_sim = sum(data * com_weight[i,j])
-                mark_n = i
-                mark_m = j
-    return mark_n , mark_m
 
-#得到神经元的N邻域
-def getNeibor(n , m , N_neibor , com_weight):
-    res = []
-    nn,mm , _ = np.shape(com_weight)
-    for i in range(nn):
-        for j in range(mm):
-            N = int(((i-n)**2+(j-m)**2)**0.5)
-            if N<=N_neibor:
-                res.append((i,j,N))
+def getWinner(data, com_weight):
+    """
+    得到获胜神经元的索引值
+    :param data:
+    :param com_weight:
+    :return:
+    """
+    n, m, d = np.shape(com_weight)
+    mat = np.zeros((n, m))
+    for i in range(d):
+        mat += data[i] * com_weight[:, :, i]
+    arg = np.argmax(mat)
+    return arg // m, arg % m
+
+def getNeibor(n, m, N_neibor, com_weight):
+    """
+    得到神经元的N邻域
+    :param n:
+    :param m:
+    :param N_neibor:
+    :param com_weight:
+    :return:
+    """
+    nn,mm, _ = np.shape(com_weight)
+    i_n = range(int(max([0, n - N_neibor])), int(min([nn, n + N_neibor])),1)
+    i_m = range(int(max([0, m - N_neibor])), int(min([mm, m + N_neibor])), 1)
+    candidates = list(itertools.product(i_n, i_m))
+
+    if candidates:
+        N = (dist.cdist([[n, m]], candidates))[0]
+        res = [(candidates[ind][0], candidates[ind][1], int(N[ind])) for ind in range(len(N)) if N[ind] <= N_neibor]
+    else:
+        res = []
     return res
 
-#学习率函数
 def eta(t,N):
-    return (0.3/(t+1))* (math.e ** -N)
-
+    """
+    学习率函数
+    :param t:
+    :param N:
+    :return:
+    """
+    return (0.3/(t+1)) * (np.exp(-N))
 #SOM算法的实现
-def do_som(dataSet , com_weight, T , N_neibor):
-    '''
-    T:最大迭代次数
-    N_neibor:初始近邻数
-    '''
+def do_som(dataSet, com_weight, T, N_neibor):
+    """
+    :param dataSet:
+    :param com_weight:
+    :param T: 最大迭代次数
+    :param N_neibor: 初始近邻数
+    :return:
+    """
     for t in range(T-1):
         com_weight = normalize_weight(com_weight)
         for data in dataSet:
-            n , m = getWinner(data, com_weight)
-            neibor = getNeibor(n , m , N_neibor , com_weight)
+            n, m = getWinner(data, com_weight)
+            neibor = getNeibor(n, m, N_neibor, com_weight)
             for x in neibor:
-                j_n=x[0];j_m=x[1];N=x[2]
+                j_n, j_m, N = x
                 #权值调整
                 com_weight[j_n][j_m] = com_weight[j_n][j_m] + eta(t,N)*(data - com_weight[j_n][j_m])
             N_neibor = N_neibor+1-(t+1)/200
     res = {}
-    N , M , _ =np.shape(com_weight)
+    N, M, _ = com_weight.shape
     for i in range(len(dataSet)):
         n, m = getWinner(dataSet[i], com_weight)
         key = n*M + m
@@ -97,11 +119,20 @@ def do_som(dataSet , com_weight, T , N_neibor):
             res[key].append(i)
     return res
 
-#SOM算法主方法
 def SOM(dataSet,com_n,com_m,T,N_neibor):
-    dataSet, old_dataSet = normalize(dataSet)
-    com_weight = initCompetition(com_n,com_m,np.shape(dataSet)[1])
-    C_res = do_som(dataSet, com_weight, T, N_neibor)
+    """
+    SOM算法主方法
+    :param dataSet:
+    :param com_n:
+    :param com_m:
+    :param T:
+    :param N_neibor:
+    :return:
+    """
+    mdataSet = np.array(dataSet)
+    mdataSet = normalize(dataSet=mdataSet)
+    com_weight = initCompetition((com_n,com_m,np.shape(mdataSet)[1]))
+    C_res = do_som(mdataSet, com_weight, T, N_neibor)
     return C_res
 
 def draw(C , dataSet):
