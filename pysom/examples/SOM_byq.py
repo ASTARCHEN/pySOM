bb1cba6007c106266d2fd10694dec18b5b0a41f2
A.Star <chenxiaolong12315@163.com>
2017-12-09 18:43:39 +0800

    变压器数据测试
    Signed-off-by: A.Star <chenxiaolong12315@163.com>

--- pysom/examples/SOM_byq.py
+++ pysom/examples/SOM_byq.py
@@ -5,11 +5,11 @@ from pysom import SOM
 from matplotlib import pyplot as plt
 if __name__=="__main__":
 
-    max_itor = 100
+    max_itor = 1
     t_list = np.zeros(max_itor)
     for i in range(max_itor):
-        SOMNet = SOM()
-        dataSet = SOMNet.loadData('../../data/watermelon.txt', split_char=' ')
+        SOMNet = SOM(steps=1500,M=3,N=3)
+        dataSet = SOMNet.loadData('../../data/byq.txt', split_char=' ')
         s = time.clock()
         C_res = SOMNet.train()
         e = time.clock()
@@ -17,3 +17,4 @@ if __name__=="__main__":
     print("{}次运行耗时：{}".format(max_itor,t_list))
     print("平均耗时:{}".format(np.mean(t_list)))
     SOMNet.showCluster(plt)
+    print(SOMNet.classLabel)
